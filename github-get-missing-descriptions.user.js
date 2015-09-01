36a35e57e05afa89a6c74d4c49b5fc260bb00b50
skratchdot <git@skratchdot.com>
2015-09-01 14:58:58 -0400

    please use github-enhancement-suite instead

--- github-get-missing-descriptions.user.js
+++ github-get-missing-descriptions.user.js
@@ -4,28 +4,13 @@
 // @description    If there are missing descriptions on a Github profile page, a button will be added. When clicked, ajax requests will be made to grab the descriptions.
 // @include        https://github.com/*
 // @match          https://github.com/*
-// @require        https://gist.github.com/skratchdot/5604120/raw/_init.js
-// @require        https://gist.github.com/skratchdot/5604120/raw/get-missing-descriptions.js
 // @run-at         document-end
 // @grant          none
 // @icon           http://skratchdot.com/favicon.ico
 // @downloadURL    https://github.com/skratchdot/github-get-missing-descriptions.user.js/raw/master/github-get-missing-descriptions.user.js
 // @updateURL      https://github.com/skratchdot/github-get-missing-descriptions.user.js/raw/master/github-get-missing-descriptions.user.js
-// @version        2.0
+// @version        3.0
 // ==/UserScript==
-/*global SKRATCHDOT, document */
 
-// This code is only going to run for browsers that don't support
-// the @require annotation when executing userscripts.
-if ('undefined' === typeof SKRATCHDOT) {
-	var addScript = function (src) {
-		'use strict';
-		var script = document.createElement('script');
-		script.src = src;
-		document.body.appendChild(script);
-		document.body.removeChild(script);
-	};
-
-	// Required by: repo-filter-info
-	addScript('https://gist.github.com/skratchdot/5604120/raw/get-missing-descriptions.js');
-}
+console.log('This script has been updated and is part of the Github Enhancement Suite' +
+		' found at: https://github.com/skratchdot/github-enhancement-suite');
